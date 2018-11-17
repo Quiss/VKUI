87cb643cfc503b45b702b4bea6e276e58fe7ce4c
Andrey "Quiss" Sokolov <walfireru@gmail.com>
2018-11-17 22:05:14 +0300

    u

---
+++ src/components/ActionSheet/ActionSheet.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>ActionSheet {{ platform }} {{ baseClassNames }}</div>
+</template>
+<script>
+import getClassName from '../../helpers/getClassName';
+import { platform, ANDROID, IOS } from '../../lib/platform';
+
+const osname = platform();
+
+const baseClassNames = getClassName('ActionSheet');
+
+export default {
+    name: 'my-component',
+    data () {
+      return {
+        baseClassNames,
+        platform,
+        ANDROID,
+        IOS
+      }
+    }
+}
+<script>
---
+++ src/components/ActionSheet/index.js
@@ -0,0 +1,4 @@
+import ActionSheet from './ActionSheet'
+
+export { ActionSheet }
+export default ActionSheet
